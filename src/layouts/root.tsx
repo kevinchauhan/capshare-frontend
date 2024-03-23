89771e0c28fb602180f4f12a7f69314196bb2559
kevinchauhan <rwnkevinchauhan@gmail.com>
2024-03-23 15:17:54 +0530

    add auto token refresh

--- src/layouts/root.tsx
+++ src/layouts/root.tsx
@@ -1,6 +1,7 @@
 import { self } from '@/http/api'
 import { useAuthStore } from '@/store'
 import { useQuery } from '@tanstack/react-query'
+import { AxiosError } from 'axios'
 import { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 
@@ -13,7 +14,13 @@ const Root = () => {
     const { setUser } = useAuthStore()
     const { data, isLoading } = useQuery({
         queryKey: ['self'],
-        queryFn: getSelf
+        queryFn: getSelf,
+        retry: (failureCount: number, error) => {
+            if (error instanceof AxiosError && error.response?.status === 401) {
+                return false
+            }
+            return failureCount < 3
+        }
     })
 
     useEffect(() => {
