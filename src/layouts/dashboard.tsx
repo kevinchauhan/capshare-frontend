7c63fe3b5c61f7227934d9fd97911222ccff414a
kevinchauhan <rwnkevinchauhan@gmail.com>
2024-03-25 16:48:41 +0530

    fix unneccessary redirect

--- src/layouts/dashboard.tsx
+++ src/layouts/dashboard.tsx
@@ -2,9 +2,25 @@ import { useAuthStore } from "@/store"
 import { Navigate, Outlet } from "react-router-dom"
 import Sidebar from "@/components/custom/sidebar"
 import Header from "@/components/custom/header"
+import { useState, useEffect } from "react"
 
 const Dashboard = () => {
-    const { user } = useAuthStore()
+    const { user } = useAuthStore();
+    const [isUserLoaded, setIsUserLoaded] = useState(false); // Track user loading state
+
+    useEffect(() => {
+        // Check if user is already available, otherwise set isUserLoaded to true after a brief delay
+        if (user !== null) {
+            setIsUserLoaded(true);
+        } else {
+            setTimeout(() => setIsUserLoaded(true), 100); // Adjust delay as needed
+        }
+    }, [user]); // Run effect when user changes
+
+    if (!isUserLoaded) {
+        return null; // Return nothing while waiting for user state
+    }
+
     if (user === null) {
         return <Navigate to={'/auth/login'} replace={true} />
     }
